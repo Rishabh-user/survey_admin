4056e1c2fa7ad7cddcaed4e6e3218af73a642ed6
nimeesha1 <nimeesha@targeticon.com>
2024-06-13 14:56:21 +0530

    [token-remove]

--- src/app/user-pages/site-login/site-login.component.ts
+++ src/app/user-pages/site-login/site-login.component.ts
@@ -17,32 +17,27 @@ export class SiteLoginComponent {
   }
 
   token = this.themeService.token
+  sitetoken:any
 
 
   ngOnInit(): void {
-    //debugger
-    //console.log("token", this.token); 
 
     if (!localStorage.getItem('authToken')) {
       if (this.token) {
+        localStorage.removeItem('authToken');
         localStorage.setItem('authToken', this.token);
 
-        // Check the refresh count
         const refreshCount = parseInt(localStorage.getItem('refreshCount') || '0', 10);
 
         if (refreshCount < 2) {
-          // Increment the refresh count and store it
           localStorage.setItem('refreshCount', (refreshCount + 1).toString());
 
-          // Delay the page refresh by 2 seconds (2000 milliseconds)
           setTimeout(() => {
             location.reload();
           }, 2000);
         } else {
-          // Reset the refresh count and navigate to the dashboard
           localStorage.setItem('refreshCount', '0');
 
-          // Delay the redirection by 2 seconds (2000 milliseconds)
           setTimeout(() => {
             const url = `/dashboard`;
             this.router.navigateByUrl(url);
@@ -53,13 +48,12 @@ export class SiteLoginComponent {
         console.error('Token is not defined.');
       }
     } else {
-      // If the authToken is already present, reset the refresh count
       localStorage.setItem('refreshCount', '0');
     }
 
+    this.sitetoken = this.route.snapshot.queryParamMap.get('token');
+    console.log("sitetoken",this.sitetoken)
 
-    //debugger
-    // Call getMyAccount() regardless of localStorage state
     this.getMyAccount();
   }
 
@@ -70,6 +64,10 @@ export class SiteLoginComponent {
     this.themeService.GetMyAccount(this.userId).subscribe((data: any) => {
 
     });
+  }
+
+  navigateToSiteLogin(token: string): void {
+    this.router.navigate(['/user-pages/site-login'], { queryParams: { token: token } });
   }
 
 }
