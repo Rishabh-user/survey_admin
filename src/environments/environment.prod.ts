ae68ce002cb957ec68c6adddee8a3a4ba58911d5
nimeesha1 <nimeesha@targeticon.com>
2024-09-16 12:08:46 +0530

    [recaptcha]

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,11 @@ export const environment = {
     apiUrl: 'https://scrip8api.opinionest.com/',
     //apiUrl: 'https://adminapibeta.angular.opinionest.com/',
     CryptoKey: 'SurveyAdmin@2023',
+    // recaptcha: {
+    //     siteKey: '6Ld7L-opAAAAAF__s_8mdxiRQteF4G17uy0nxOdF',
+    // },
     recaptcha: {
-        siteKey: '6Ld7L-opAAAAAF__s_8mdxiRQteF4G17uy0nxOdF',
+        siteKey: '6LenREUqAAAAAAlZEUAkqOTDfJzRXy_uX6oCN9Lo',
     },
     baseURL: '/manage',
     mainURL: 'https://scrip8.trackopinion.com/',
