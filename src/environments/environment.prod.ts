9b97443d40187599b0ab85d6bf5b8911588d090d
nimeesha1 <nimeesha@targeticon.com>
2024-09-24 17:20:21 +0530

    [planid]

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,12 @@ export const environment = {
     apiUrl: 'https://scrip8api.opinionest.com/',
     //apiUrl: 'https://adminapibeta.angular.opinionest.com/',
     CryptoKey: 'SurveyAdmin@2023',
-    // recaptcha: {
-    //     siteKey: '6Ld7L-opAAAAAF__s_8mdxiRQteF4G17uy0nxOdF',
-    // },
     recaptcha: {
-        siteKey: '6LenREUqAAAAAAlZEUAkqOTDfJzRXy_uX6oCN9Lo',
+        siteKey: '6Ld7L-opAAAAAF__s_8mdxiRQteF4G17uy0nxOdF',
     },
+    // recaptcha: {
+    //     siteKey: '6LenREUqAAAAAAlZEUAkqOTDfJzRXy_uX6oCN9Lo',
+    // },
     baseURL: '/manage',
     mainURL: 'https://scrip8.trackopinion.com/',
 }
