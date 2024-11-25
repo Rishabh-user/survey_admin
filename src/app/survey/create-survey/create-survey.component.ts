f52fd617c26239f356d2f471a4c304a5e499e8e3
nimeesha1 <nimeesha@targeticon.com>
2024-11-25 11:38:58 +0530

    [logic]

--- src/app/survey/create-survey/create-survey.component.ts
+++ src/app/survey/create-survey/create-survey.component.ts
@@ -1434,7 +1434,7 @@ export class CreateSurveyComponent implements OnInit, AfterViewInit {
         response => {
 
           this.utils.showSuccess('Logic Created Successfully.');
-          //window.location.reload();
+          window.location.reload();
         },
         error => {
           console.error('Error occurred while sending POST request:', error);
@@ -1445,7 +1445,7 @@ export class CreateSurveyComponent implements OnInit, AfterViewInit {
       this.surveyservice.createLogic(this.questionLogic).subscribe(
         response => {
           this.utils.showSuccess('Logic Created Successfully.');
-         // window.location.reload();
+          window.location.reload();
         },
         error => {
           console.error('Error occurred while sending POST request:', error);
